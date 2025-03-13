2abf77a0c29647d234ecbd2d02c5bc310694e6d8
Max Zviahintsev <max.zviahintsev@outlook.com>
2025-03-13 15:32:25 +0200

    Set up server

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import fastify, { FastifyInstance } from 'fastify'
+import { Server, IncomingMessage, ServerResponse } from 'node:http'
+
+const app: FastifyInstance<Server, IncomingMessage, ServerResponse> = fastify({
+  logger: {
+    transport: {
+      target: '@fastify/one-line-logger',
+    },
+  },
+})
+
+app.get('/ping', async () => {
+  return 'pong\n'
+})
+
+export default app
---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import app from './app.ts'
+
+const PORT = Number(process.env.PORT) || 8080
+const HOST = '0.0.0.0'
+
+app.listen({ port: PORT, host: HOST }, (err, address) => {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
+  console.log(`Server listening at ${address}`)
+})
